74ec68bde71e039a49076a60f05fbf4f1b3f7785
ayan <ayan02472@gmail.com>
2025-12-28 20:17:23 +0530

    Base code for NATS with common functions

--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module github.com/AyanNandaGoswami/microservice-common-utilities
 
 go 1.24.0
 
-require github.com/golang-jwt/jwt/v5 v5.2.1
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/nats-io/nats.go v1.48.0
+)
+
+require (
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/nats-io/nkeys v0.4.11 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+)
---
+++ v1/utilities/nats.go
@@ -0,0 +1,78 @@
+package utilities
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/AyanNandaGoswami/microservice-common-utilities/v1/models"
+	"github.com/nats-io/nats.go"
+)
+
+var nc *nats.Conn // global connection
+
+// InitializeNATS connects to NATS
+func InitializeNATS() {
+	var err error
+
+	natsURL := os.Getenv("NATS_URI")
+	if natsURL == "" {
+		natsURL = nats.DefaultURL
+	}
+
+	nc, err = nats.Connect(natsURL)
+	if err != nil {
+		log.Fatalf("Error connecting to NATS: %v", err)
+	}
+	log.Println("Connected to NATS")
+}
+
+// CloseNATS closes the NATS connection gracefully
+func CloseNATS() {
+	if nc != nil && !nc.IsClosed() {
+		nc.Drain() // Waits for pending messages before closing
+		log.Println("Draining NATS connection...")
+		nc.Close()
+		log.Println("NATS connection closed cleanly")
+	}
+}
+
+// RequestAndParse sends a NATS request and parses the response into targetedStruct
+func RequestAndParse(subject string, payload interface{}, targetedStruct interface{}) error {
+	// Marshal request payload
+	dataBytes, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("failed to marshal request payload: %w", err)
+	}
+
+	// Send request
+	msg, err := nc.Request(subject, dataBytes, nats.DefaultTimeout)
+	if err != nil {
+		return fmt.Errorf("request failed: %w", err)
+	}
+
+	// Parse NATS response into targetedStruct
+	if err := models.ParseNatMsgToStruct(msg, targetedStruct); err != nil {
+		return fmt.Errorf("failed to parse NATS response: %w", err)
+	}
+
+	return nil
+}
+
+// Reply sends a NATSResponse as a reply to the given NATS message
+func Reply(response models.NATSResponse, msg *nats.Msg) {
+	// marshal response
+	data, err := json.Marshal(response)
+	if err != nil {
+		log.Printf("Failed to marshal response: %v", err)
+		return
+	}
+
+	// send reply
+	if err := msg.Respond(data); err != nil {
+		log.Printf("Failed to send reply: %v", err)
+	} else {
+		log.Printf("Sent reply to '%s' with data %s", msg.Subject, string(data))
+	}
+}
